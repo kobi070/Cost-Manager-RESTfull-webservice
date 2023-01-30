0c72cfcec2f7d85602fdc686e98843be0ce78c40
kobi070 <kobi070@gmail.com>
2023-01-30 13:02:13 +0200

    7th commit -v 1.1

--- routes/api.js
+++ routes/api.js
@@ -3,44 +3,41 @@ const router = express.Router();
 const Costs = require('../models/costs');
 const Users = require('../models/users');
 
-// Dummy user we were asked to create
-dummyUser = {
-    _id : 123123,
-    first_name: 'moshe',
-    last_name: 'israeli',
-    birthday: new Date(Date.parse("January, 10, 1990")).toLocaleDateString("en-us", {
-      month: "long",
-      day: "numeric",
-      year: "numeric"
-    }).toString(),
-}
 
+async function insertUser() {
+  try {
+    const dummyUser = {
+      _id : 123123,
+      first_name: 'moshe',
+      last_name: 'israeli',
+      birthday: new Date(Date.parse("January, 10, 1990")).toLocaleDateString("en-us", {
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+      }).toString(),
+    };
 
-// Insirtion of the dummy
-Users.insertOne(dummyUser).then( (dummyUser) => {
+    const insertedUser = await Users.insertOne(dummyUser);
     console.log("insertOne method for UsersSchema");
-    
-    console.log(`A user ${dummyUser} has been created`);
-}).catch((error) => {
+    console.log(`A user ${insertedUser} has been created`);
+  } catch (error) {
     console.log(error);
-});
+  }
+}
+insertUser();
 
-router.get('/addcost', (req, res) => {
-    console.log("GET method for /addcost");
-    res.render('addcost');
-});
 
-router.post('/addcost', (req, res, next) =>{
+//POST method for adding cost using url parms at => addcost/:user_id/:year/:month/:day/:description/:category/:sum
+router.post('/addcost/:user_id/:year/:month/:day/:description/:category/:sum', (req, res, next) =>{
     
     console.log("POST method for /addcost");
-    const { year, month, day, description, category, sum} = req.body;
+    const { year, month, day, description, category, sum} = req.params;
     var user_id = 123123;
-    console.log(`${user_id}`);
 
     if ( !year || !month || !day || !description || !category || !sum){
         return res.status(400).json({ error : 'Missing required parms'});
     }
-    
+    console.log(category);
     if (["food", "health", "housing", "sport", "education", "transportation", "other"].indexOf(category) === -1) {
         return res.status(400).json({ error: "Invalid category" });
     }
@@ -58,10 +55,9 @@ router.post('/addcost', (req, res, next) =>{
     Costs.insertOne(newCosts).then( (newCosts) => {
         res.send(`new cost: ${newCosts} has been created`);
     }).catch(next);
-    // res.render('addcosts');
 });
 
-router.get('/about', (res) => {
+router.get('/about', (req, res) => {
     console.log("GET method for /about");
     const devs = [
         {first_name: 'Kobi', last_name: 'Kuzi', id: 316063908, email: 'Kobi070@gmail.com'},
@@ -74,7 +70,7 @@ router.get('/report/:user_id/:year/:month', (req, res) => {
   const {user_id, year, month} = req.params;
   // console.log(req.params);
 
-  result = {};
+  const result = {};
   
   Costs.find({user_id, year, month}, (err, costs) => {
     if(err) console.log(`this is err : ${err}`);
@@ -82,7 +78,7 @@ router.get('/report/:user_id/:year/:month', (req, res) => {
         const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
         const index = Costs.prototype.getPropertiesList();
 
-        const result = {};
+        // const result = {};
 
         categories.forEach(c => {
           result[c] = [];
@@ -114,79 +110,5 @@ router.get('/report/:user_id/:year/:month', (req, res) => {
     });
 });
 
-// Testing GET and POST functions
-
-
-router.get('/report/:year/:month/:user_id', (req, res) => {
-  // Extract the parameters from the request object
-  const { year, month, user_id } = req.params;
-
-  // Use Mongoose to query the database for the specific month and year and user_id
-  Costs.find({year, month, user_id} , (err, costs) => {
-    if (err) {
-      // Handle the error
-      res.status(500).send(err);
-    } else {
-      // Create an object to store the costs by category
-      const report = {};
-      console.log(costs);
-      
-      // Iterate over the costs and group them by category
-      costs.forEach((cost) => {
-        if (!report[cost.category]) {
-          report[cost.category] = [];
-        }
-        report[cost.category].push(cost);
-      });
-
-      // Send the report as a JSON response
-      res.json(report);
-    }
-  });
-});
-
-router.get('/testing/:user_id', (req, res) => {
-  const {user_id} = req.params;
-  // console.log(req.params);
-
-  result = {};
-  
-  Costs.find({user_id}, (err, costs) => {
-    if(err) console.log(`this is err : ${err}`);
-    else{
-        const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
-        const index = Costs.prototype.getPropertiesList();
-
-        const result = {};
-
-        categories.forEach(c => {
-          result[c] = [];
-        });
-
-        Object.entries(costs).forEach(([key, value]) => {
-          
-          let category = value.category;
-          
-          if (categories.includes(category)){
-            let costDetails = {};
-
-            for (let val in value){
-              if (index.includes(val))
-              {
-                if (val != "category")
-                {
-                  costDetails[val] = value[val];
-                }
-              }
-            }
-            result[category].push(costDetails);
-          }
-        });
-        console.log(result);
-      }
-      res.json(result);
-      
-    });
-});
 
 module.exports = router;
