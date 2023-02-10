3a6a5d81f8e389bb078fb864bafb9337c22793f2
kobi070 <kobi070@gmail.com>
2023-02-10 03:49:47 +0200

    final commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// Kobi Kuzi 316063908
+// Dan Kvitca 205570674,
+
 const createError = require('http-errors');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+// const logger = require('morgan');
+const winston = require('winston');
 const session = require('express-session');
+
+
+// Define the Winston logging configuration
+const winstonLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -21,7 +37,7 @@ mongoose.Promise = global.Promise;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -34,6 +50,10 @@ app.use(session({
     maxAge: 3000
   }
 }));
+app.use(function (req, res, next) {
+  winstonLogger.info(`${req.method} ${req.originalUrl}`);
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -46,6 +66,15 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+  // Log the error message using Winston
+  winstonLogger.error(err.message);
+  if (process.env.NODE_ENV !== 'production'){
+    winstonLogger.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }))
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,27 @@
+// Kobi Kuzi 316063908
+// Dan Kvitca 205570674,
+
 const express = require('express');
 const router = express.Router();
 const Costs = require('../models/costs');
 const Users = require('../models/users');
+const winston = require('winston');
 
+const winstonLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
 
+// Function to insert a dummy user into the Users collection
 async function insertUser() {
   try {
+
+    // Define the dummy user object
     const dummyUser = {
       _id : 123123,
       first_name: 'moshe',
@@ -16,34 +32,41 @@ async function insertUser() {
         year: "numeric"
       }).toString(),
     };
-
+    // Try to insert the dummy user into the Users collection
     const insertedUser = await Users.insertOne(dummyUser);
-    console.log("insertOne method for UsersSchema");
-    console.log(`A user ${insertedUser} has been created`);
+    winstonLogger.info("Inserted a new user into the Users collection.");
+    winstonLogger.info(`A user with the following details has been created: ${insertedUser}`);
   } catch (error) {
-    console.log(error);
+    winstonLogger.error(`An error occurred while trying to insert a new user: ${error}`);
   }
 }
+
+// Call the insertUser function
 insertUser();
 
-
-
-
-//POST method for adding cost using url parms at => addcost/:user_id/:year/:month/:day/:description/:category/:sum
+// POST method to add cost using URL parameters (user_id, year, month, day, description, category, sum)
 router.post('/addcost/:user_id/:year/:month/:day/:description/:category/:sum', (req, res, next) =>{
+  
+    // Log that a POST request has been received for the /addcost endpoint
+    winstonLogger.info("Received a POST request for /addcost endpoint.");
     
-    console.log("POST method for /addcost");
+    // Destructure the parameters from the request
     const { year, month, day, description, category, sum} = req.params;
-    var user_id = 123123;
-
+  
+    // Set user_id by dummy
+    var user_id = dummyUser.user_id;
+    
+    // Check if any required parameters are missing
     if ( !year || !month || !day || !description || !category || !sum){
         return res.status(400).json({ error : 'Missing required parms'});
     }
-    console.log(category);
+    
+    // Check if the category is valid
     if (["food", "health", "housing", "sport", "education", "transportation", "other"].indexOf(category) === -1) {
         return res.status(400).json({ error: "Invalid category" });
     }
     
+    // Define the newCosts object
     const newCosts = {
         user_id,
         year,
@@ -53,65 +76,89 @@ router.post('/addcost/:user_id/:year/:month/:day/:description/:category/:sum', (
         category,
         sum
     }
-
-    Costs.insertOne(newCosts).then( (newCosts) => {
-        res.send(`new cost: ${newCosts} has been created`);
-    }).catch(next);
+    
+    // Try to insert the newCosts object into the Costs collection
+    Costs.insertOne(newCosts).then( (newCosts) => {      
+        // Log that a new cost document has been created
+        winstonLogger.info(`A new cost document has been created: ${newCosts}`);
+        // Send the new cost object back to the client
+        res.send(`A new cost with the following details has been created: ${newCosts}`);
+    }).catch(error => {      
+      // log an error if it occurred while trying to insert a new cost
+      winstonLogger.error(`An error occurred while trying to insert a new cost: ${error}`);
+      // pass the error to the next middleware
+      next(error);
+    });
 });
 // GET method for /about => returns information about the developers
 router.get('/about', (req, res) => {
-    console.log("GET method for /about");
-    console.log(`session created successfully at id => ${req.session.id}`);
-    
+    winstonLogger.info("GET method for /about");
+    winstonLogger.info(`Session created successfully with ID: ${req.session.id}`);
     const devs = [
         {first_name: 'Kobi', last_name: 'Kuzi', id: 316063908, email: 'Kobi070@gmail.com'},
         {first_name: 'Dan', last_name: 'Kvitca', id: 205570674, email: 'Dkvitca@gmail.com'}
     ]
     res.json(devs);
 });
-
+// Get report for specific user by year and month
 router.get('/report/:user_id/:year/:month', (req, res) => {
+  // Destructure user_id, year, and month from the request parameters
   const {user_id, year, month} = req.params;
-  // console.log(req.params);
 
+  // Log the request parameters for debugging purposes
+  winstonLogger.info(req.params);
+
+  // Initialize an object to store the results
   const result = {};
-  
+
+  // Find costs that match the user_id, year, and month
   Costs.find({user_id, year, month}, (err, costs) => {
-    if(err) console.log(`this is err : ${err}`);
+    // If there is an error, log the error message
+    if(err) winstonLogger.error(`this is err : ${err}`);
     else{
-        const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
-        const index = Costs.prototype.getPropertiesList();
+      // Define an array of categories
+      const categories = ["food", "health", "housing", "sport", "education", "transportation", "other"];
+      // Get a list of cost properties
+      const index = Costs.prototype.getPropertiesList();
 
-        // const result = {};
+      // Initialize an empty array for each category
+      categories.forEach(c => {
+        result[c] = [];
+      });
 
-        categories.forEach(c => {
-          result[c] = [];
-        });
+      // Iterate over the costs
+      Object.entries(costs).forEach(([key, value]) => {
+        // Get the cost's category
+        let category = value.category;
 
-        Object.entries(costs).forEach(([key, value]) => {
-          
-          let category = value.category;
-          
-          if (categories.includes(category)){
-            let costDetails = {};
+        // If the category is one of the predefined categories
+        if (categories.includes(category)){
+          // Initialize an object to store the cost details
+          let costDetails = {};
 
-            for (let val in value){
-              if (index.includes(val))
+          // Iterate over the cost's properties
+          for (let val in value){
+            // If the property is one of the defined cost properties
+            if (index.includes(val))
+            {
+              // If the property is not the category
+              if (val != "category")
               {
-                if (val != "category")
-                {
-                  costDetails[val] = value[val];
-                }
+                // Add the property to the cost details object
+                costDetails[val] = value[val];
               }
             }
-            result[category].push(costDetails);
           }
-        });
-        console.log(result);
-      }
-      res.json(result);
-      
-    });
+          // Push the cost details to the result array for the category
+          result[category].push(costDetails);
+        }
+      });
+      // Log the result for debugging purposes
+      winstonLogger.info(result);
+    }
+    // Send the result as a JSON response
+    res.json(result);
+  });
 });
 
 
